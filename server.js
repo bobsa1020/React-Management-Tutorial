8fed3a1868c3804de58926292439f57b943a29e7
bobsa1020 <bobsa1020@gmail.com>
2020-07-26 16:12:56 +0900

    매니지먼트 스터디 잠시 중단

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
+const multer = require('multer');
+const upload = multer({dest: './upload'});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,5 +30,22 @@ app.get('/api/customers', (req, res) => {
       }
     );
 });
+
+app.use('/image', express.static('./upload'));
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUE (null, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+
+  connection.query(sql, params,
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
